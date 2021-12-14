d3162bb9aace453892ae5d81ab30fea6781fc547
kingsye <54584598+kingsye@users.noreply.github.com>
2021-12-14 23:49:00 +0100

    Add files via upload

---
+++ A. 3041 Problem. Find the larger number.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 05:00:04 2021
+
+@author: Administrator
+"""
+x,y = eval(input())
+c = int(x)
+d = int(y)
+if c > d:
+    print(c)
+else:
+    print(d)
